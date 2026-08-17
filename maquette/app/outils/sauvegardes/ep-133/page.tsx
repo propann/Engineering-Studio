839a31b5ebc3290cb4f2f31a80f0b476a0e5c65c
Enzo <54742679+propann@users.noreply.github.com>
2026-08-17 14:36:00 +0200

    Add machine backup center

---
+++ maquette/app/outils/sauvegardes/ep-133/page.tsx
@@ -0,0 +1,2 @@
+import BackupMachinePage from "../BackupMachinePage";
+export default function EP133Backup(){return <BackupMachinePage machine="ep-133"/>}
---
+++ maquette/app/outils/sauvegardes/op-1/page.tsx
@@ -0,0 +1,2 @@
+import BackupMachinePage from "../BackupMachinePage";
+export default function OP1Backup(){return <BackupMachinePage machine="op-1"/>}
